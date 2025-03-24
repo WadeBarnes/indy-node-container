bb020f6d3794fa058e1b0dd2a8a480478ed5e4c6
Wade Barnes <wade@neoterictech.ca>
2025-03-24 14:48:48 +0000

    Update configurtion for use with Sovrin MainNet.
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

---
+++ run/SovrinMainNet02/etc_indy/indy_config.py
@@ -0,0 +1,43 @@
+import os
+import logging
+logLevel = getattr(logging, os.getenv("LOG_LEVEL", "INFO").upper())
+
+# Current network
+NETWORK_NAME = "live"
+
+# Disable stdout logging
+enableStdOutLogging = False
+
+# Directory to store ledger.
+LEDGER_DIR = '/var/lib/indy'
+
+# Directory to store logs. You might want to mount this in order to access the log files from outside the container.
+LOG_DIR = '/var/log/indy'
+
+# Directory to store keys.
+KEYS_DIR = '/var/lib/indy'
+
+# Directory to store genesis transactions files.
+GENESIS_DIR = '/var/lib/indy'
+
+# Directory to store backups.
+BACKUP_DIR = '/var/lib/indy/backup'
+
+# Directory to store plugins.
+PLUGINS_DIR = '/var/lib/indy/plugins'
+
+# Directory to store node info.
+NODE_INFO_DIR = '/var/lib/indy'
+
+UPGRADE_ENTRY = 'sovrin'
+ENABLED_PLUGINS = []
+ENABLED_PLUGINS.append('sovtoken')
+ENABLED_PLUGINS.append('sovtokenfees')
+
+INDY_VERSION_MATCHING = {"1.1.52": "1.9.1", "1.1.24": "0.9.3", "1.1.35": "0.9.3", "1.1.50": "1.0.0"}
+
+# For running indy >= 1.13 in a legacy network (i.e. including revocation transactions written by indy nodes < 1.13)
+REV_STRATEGY_USE_COMPAT_ORDERING = True
+
+MAX_CONNECTED_CLIENTS_NUM=2200
+controlServiceHost='SovrinMainNet02-controller'
